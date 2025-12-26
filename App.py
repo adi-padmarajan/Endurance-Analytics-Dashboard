4142fbab59b4fef2a1b37c7937d7fa9502662326
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-12-26 08:48:47 -0800

    Updated App.py (loaded Datasets, displayed datasets etc)

--- App.py
+++ App.py
@@ -2,15 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Set page config for dashboard look
-st.set_page_config(
-    page_title="The 80-Minute Journey",
-    page_icon="🏆",
-    layout="wide",
-    initial_sidebar_state="auto"
-)
+st.title("The 80 Minutes Journey")
+st.write("A Data-Driven Analysis of Marathon Progression (Feb 2022 – Oct 2025)")
 
 # Load datasets
 activities_df = pd.read_csv("activities_dataset.csv")
 challenges_df = pd.read_csv("global_challenges.csv")
 
+# Display both CSV files in Streamlit
+with st.expander("Show activities_dataset.csv"):
+	st.dataframe(activities_df)
+
+with st.expander("Show global_challenges.csv"):
+	st.dataframe(challenges_df)
